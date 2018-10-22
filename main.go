04ac2bd61f797014ef40cf4411857b98bf031e6f
Sam Mulube <sam@umbrellium.co.uk>
2018-10-22 18:02:06 +0100

    Try removing the custom import, and replace for this fork

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/golang/protobuf/proto"
 	plugin "github.com/golang/protobuf/protoc-gen-go/plugin"
-	"go.larrymyers.com/protoc-gen-twirp_typescript/generator"
+	"github.com/smulube/protoc-gen-twirp_typescript/generator"
 )
 
 func main() {
